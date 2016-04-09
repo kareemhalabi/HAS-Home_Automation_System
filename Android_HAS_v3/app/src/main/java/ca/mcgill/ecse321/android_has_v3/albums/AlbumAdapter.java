923b2d64f397491c94cf0530001049046810a290
kareemhalabi <kareemhalabi24@gmail.com>
2016-04-09 19:28:37 -0400

    Abstract away list view item

--- Android_HAS_v3/app/src/main/java/ca/mcgill/ecse321/android_has_v3/albums/AlbumAdapter.java
+++ Android_HAS_v3/app/src/main/java/ca/mcgill/ecse321/android_has_v3/albums/AlbumAdapter.java
@@ -40,24 +40,24 @@ public class AlbumAdapter extends BaseAdapter{
 
         View vi = convertView;
         if(vi == null)
-            vi = inflater.inflate(R.layout.listview_item_album, null);
+            vi = inflater.inflate(R.layout.listview_item, null);
 
         Album album = albums.get(position);
 
-        TextView albumName = (TextView) vi.findViewById(R.id.album_nameTextView);
+        TextView albumName = (TextView) vi.findViewById(R.id.top_left_TextView);
         albumName.setText(album.getName());
 
         //Formats date as "DD-MM-YYYY"
-        TextView releaseDate = (TextView) vi.findViewById(R.id.album_releaseTextView);
+        TextView releaseDate = (TextView) vi.findViewById(R.id.top_right_TextView);
         Calendar c = Calendar.getInstance();
         c.setTime(album.getReleaseDate());
         releaseDate.setText(String.format("%02d-%02d-%04d", c.get(Calendar.DAY_OF_MONTH),
                 c.get(Calendar.MONTH) + 1, c.get(Calendar.YEAR)));
 
-        TextView mainArtist = (TextView) vi.findViewById(R.id.album_artistTextView);
+        TextView mainArtist = (TextView) vi.findViewById(R.id.bottom_left_TextView);
         mainArtist.setText(album.getMainArtist().getName());
 
-        TextView genre = (TextView) vi.findViewById(R.id.album_genreTextView);
+        TextView genre = (TextView) vi.findViewById(R.id.bottom_right_TextView);
         genre.setText(album.getGenre());
 
         return vi;
